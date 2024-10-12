4c50e0622b76edb2e75666676f254be0d452a61c
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-10-12 13:53:47 +1100

    Initial commit

---
+++ src/Plugin.cs
@@ -0,0 +1,25 @@
+using BepInEx;
+using RoR2;
+
+namespace itsschwer.Items
+{
+    [R2API.Utils.NetworkCompatibility(R2API.Utils.CompatibilityLevel.EveryoneMustHaveMod, R2API.Utils.VersionStrictness.EveryoneNeedSameModVersion)]
+    [BepInDependency(R2API.ItemAPI.PluginGUID)]
+    [BepInPlugin(GUID, GUID, Version)]
+    public sealed class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = Author + ".Items";
+        public const string Author = "itsschwer";
+        public const string Version = "0.0.0";
+
+        internal static new BepInEx.Logging.ManualLogSource Logger { get; private set; }
+
+        private void Awake()
+        {
+            // This plugin's Plugin.Name is the same as Plugin.GUID
+            Logger = base.Logger;
+
+            Logger.LogMessage("~awake.");
+        }
+    }
+}
