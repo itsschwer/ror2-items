0267e1121e27a369c8070b0cea6f4432b2b2379b
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-10-12 21:33:35 +1100

    Update Plugin.cs

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -11,7 +11,7 @@ namespace itsschwer.Items
         public const string GUID = Author + ".Items";
         public const string Author = "itsschwer";
         public const string Name = "Items_itsschwer"; // only to satisfy release workflow
-        public const string Version = "0.0.0";
+        public const string Version = "1.0.0";
 
         internal static new BepInEx.Logging.ManualLogSource Logger { get; private set; }
 
